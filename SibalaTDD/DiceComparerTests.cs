546a13a60207da56bc3ac8bacc89042aa464b58e
Joey Chen <jay.hatelove@gmail.com>
2017-12-15 00:12:58 +0800

    add failed test: normal points should be larger than no points

--- SibalaTDD/DiceComparerTests.cs
+++ SibalaTDD/DiceComparerTests.cs
@@ -11,6 +11,17 @@ namespace SibalaTDD
             FirstShouldBeEqualToSecond(new Dice(new[] { 1, 4, 3, 2 }), new Dice(new[] { 6, 5, 3, 4 }));
         }
 
+        [TestMethod]
+        public void NormalPoints_should_be_larger_than_NoPoints()
+        {
+            FirstShouldBeLargerThanSecond(new Dice(new[] { 2, 2, 4, 3 }), new Dice(new[] { 6, 5, 3, 4 }));
+        }
+
+        private void FirstShouldBeLargerThanSecond(Dice x, Dice y)
+        {
+            Assert.IsTrue(new DiceComparer().Compare(x, y) > 0);
+        }
+
         private static void FirstShouldBeEqualToSecond(Dice first, Dice second)
         {
             Assert.IsTrue(new DiceComparer().Compare(first, second) == 0);
