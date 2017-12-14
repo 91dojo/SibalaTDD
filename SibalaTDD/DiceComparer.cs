182312107f3209d9dc094a21585e352370be039e
Joey Chen <jay.hatelove@gmail.com>
2017-12-15 00:09:20 +0800

    add failed test: DiceComparer, 2 no points dices should be equal

---
+++ SibalaTDD/DiceComparer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SibalaTDD
+{
+    public class DiceComparer : IComparer<Dice>
+    {
+        public int Compare(Dice x, Dice y)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ SibalaTDD/DiceComparerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SibalaTDD
+{
+    [TestClass]
+    public class DiceComparerTests
+    {
+        [TestMethod]
+        public void NoPoints_1432_equal_to_6534()
+        {
+            var x = new Dice(new[] { 1, 4, 3, 2 });
+            var y = new Dice(new[] { 6, 5, 3, 4 });
+
+            var comparer = new DiceComparer();
+            Assert.IsTrue(comparer.Compare(x, y) == 0);
+        }
+    }
+}
