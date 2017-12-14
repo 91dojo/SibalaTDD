e38971e2a3a1af2a343041ba935df90cb2a0e6ec
Joey Chen <jay.hatelove@gmail.com>
2017-12-15 00:04:47 +0800

    refactor: convert Points, MaxPoint, Type, Output to auto-property

--- SibalaTDD/Dice.cs
+++ SibalaTDD/Dice.cs
@@ -6,10 +6,6 @@ namespace SibalaTDD
     public class Dice
     {
         public List<int> _dices;
-        private int _points;
-        private int _maxPoint;
-        private DiceType _diceType;
-        private string _output;
 
         public Dice(int[] dices)
         {
@@ -34,28 +30,12 @@ namespace SibalaTDD
             return diceHandlerLookup[_dices.GroupBy(x => x).Max(x => x.Count())];
         }
 
-        public int Points
-        {
-            get { return this._points; }
-            set { this._points = value; }
-        }
+        public int Points { get; set; }
 
-        public int MaxPoint
-        {
-            get { return this._maxPoint; }
-            set { this._maxPoint = value; }
-        }
+        public int MaxPoint { get; set; }
 
-        public DiceType Type
-        {
-            get { return this._diceType; }
-            set { this._diceType = value; }
-        }
+        public DiceType Type { get; set; }
 
-        public string Output
-        {
-            get { return this._output; }
-            set { this._output = value; }
-        }
+        public string Output { get; set; }
     }
 }
